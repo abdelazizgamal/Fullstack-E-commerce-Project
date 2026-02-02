f18685047e55489f79668c87f398be5f640c53fc
youssef-hesham <youss4923@gmail.com>
2026-02-02 15:05:21 +0200

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Register } from './Components/Register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Register}
+];
