43bc39b398215ca3e7127e037335984d03f50921
abdelazizgamal <abdelazizgamal67@gmail.com>
2026-02-04 08:10:30 +0200

    Register-SignUp V1

--- src/app/Components/Register/register.ts
+++ src/app/Components/Register/register.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { validate } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
-
+signupForm = new FormGroup({
+  fullName: new FormControl('', [Validators.required]),
+  email: new FormControl('', [Validators.required, Validators.email]),
+  password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  confirmPassword: new FormControl('',[Validators.required])
+  })
 }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { routes } from './app.routes';
+import { Register } from "./Components/Register/register";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Register],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
