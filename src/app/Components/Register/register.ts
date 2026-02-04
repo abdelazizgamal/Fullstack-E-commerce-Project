2ae87bb59d9289e08d4f534dbc28884296adfe72
abdelazizgamal <abdelazizgamal67@gmail.com>
2026-02-05 00:41:56 +0200

    Signup V2

--- src/app/Components/Register/register.ts
+++ src/app/Components/Register/register.ts
@@ -9,10 +9,52 @@ import { validate } from '@angular/forms/signals';
   styleUrl: './register.css',
 })
 export class Register {
-signupForm = new FormGroup({
-  fullName: new FormControl('', [Validators.required]),
-  email: new FormControl('', [Validators.required, Validators.email]),
-  password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-  confirmPassword: new FormControl('',[Validators.required])
-  })
+
+  showCountryDropdown = false;
+
+  countries: string[] = [
+    'Egypt',
+    'Saudi Arabia',
+    'UAE',
+    'Kuwait',
+    'Qatar'
+  ];
+
+   signupForm = new FormGroup({
+    fullName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    country: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('',[Validators.required])
+    })
+
+  selectedCountry: string | null = null;
+  toggleCountryDropdown() {
+    this.showCountryDropdown = !this.showCountryDropdown;
+  }
+
+  selectCountry(country: string) {
+    this.selectedCountry = country;
+    this.showCountryDropdown = false;
+
+    this.signupForm.patchValue({
+      country: country
+    });
+  }
+
+
+
+  //////////////////////////////////////////
+  showPassword = false;
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  showPassword2 = false;
+  togglePassword2() {
+    this.showPassword2 = !this.showPassword2;
+  }
+
+
 }
