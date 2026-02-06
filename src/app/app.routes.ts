b9000a8434d05190cd818a890b956b6b1ebecc25
youssef-hesham <youss4923@gmail.com>
2026-02-06 15:57:00 +0200

    fix login style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Login } from './Components/login/login';
 import { Register } from './Components/Register/register';
 
 export const routes: Routes = [
-  {path: '', component: Register}
+    {path: '', component: Register},
 
     {path: 'login', component: Login}
 ];
