6e4c93d3c189877a16ad7d7c726a617ac80e3f96
abdelazizgamal <abdelazizgamal67@gmail.com>
2026-02-08 09:50:14 +0200

    profile component, done!.

--- src/app/Components/login/login.ts
+++ src/app/Components/login/login.ts
@@ -11,71 +11,60 @@ import { find, map } from 'rxjs';
   styleUrl: './login.css',
 })
 export class Login {
-
-  LoginObj:LoginModel = new LoginModel();
+  LoginObj: LoginModel = new LoginModel();
   showPassword = false;
 
   constructor(
     private usersService: Users,
-    private router : Router
-  ) {
-  }
-
-
+    private router: Router,
+  ) {}
 
   togglePassword() {
     this.showPassword = !this.showPassword;
   }
 
   LoginButton() {
-      // (GET)  make sure that user exists.
-      const fakeToken = crypto.randomUUID() + crypto.randomUUID();
-      this.usersService.get_user().pipe(
-        map(users => users.find((user:any) =>
-          user.email === this.LoginObj.email &&
-          user.password === this.LoginObj.password
-        ))
-      ).subscribe(user => {
+    // (GET)  make sure that user exists.
+    const fakeToken = crypto.randomUUID() + crypto.randomUUID();
+    this.usersService
+      .get_user()
+      .pipe(
+        map((users) =>
+          users.find(
+            (user: any) =>
+              user.email === this.LoginObj.email && user.password === this.LoginObj.password,
+          ),
+        ),
+      )
+      .subscribe((user) => {
         if (user) {
           debugger;
-          // alert('Done')
+
           localStorage.setItem('token', fakeToken);
+          localStorage.setItem('currentUser', JSON.stringify(user)); // 👈 ADD THIS
+
           console.log('Login successful', user);
           this.router.navigate(['/profile']);
         } else {
-          alert('Invalid email or password')
+          alert('Invalid email or password');
           console.log('Invalid email or password');
         }
       });
 
-      // this.usersService.Login(this.LoginObj).subscribe(res => {
-      //   localStorage.setItem('token', res.accessToken); // store token
-      //   console.log('Logged in successfully');
-      // }, err => {
-      //   console.log('Login failed', err);
-      // });
-
-
-
-
-
-
-
-
-    }
-
-
-
-
+    // this.usersService.Login(this.LoginObj).subscribe(res => {
+    //   localStorage.setItem('token', res.accessToken); // store token
+    //   console.log('Logged in successfully');
+    // }, err => {
+    //   console.log('Login failed', err);
+    // });
+  }
 }
 
-
 class LoginModel {
-    email:string;
-    password:string;
+  email: string;
+  password: string;
 
   constructor() {
-    this.email = "",
-    this.password = ""
+    ((this.email = ''), (this.password = ''));
   }
 }
